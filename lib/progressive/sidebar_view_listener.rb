8d19138841dda8d225c144c93d661d3e2c3143eb
Dmitry Babenko <dmitry@babenko.in.ua>
2013-12-18 12:03:42 +0200

    Rename sidebar views to isolate name

--- lib/progressive/sidebar_view_listener.rb
+++ lib/progressive/sidebar_view_listener.rb
@@ -1,3 +1,3 @@
 class Progressive::SidebarViewListener < Redmine::Hook::ViewListener
-  render_on :view_layouts_base_sidebar, :partial => "progressive_sidebar"
+  render_on :view_layouts_base_sidebar, :partial => "progressive_gantt_sidebar"
 end
